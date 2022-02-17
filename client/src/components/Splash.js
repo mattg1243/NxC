fdc5b741d5a8fc8f3f864839a31b2230509e19d2
Matt Gallucci <mattgallucci97@gmail.com>
2022-02-17 15:30:35 -0800

    logo assets added for splash page

--- client/src/components/Splash.js
+++ client/src/components/Splash.js
@@ -11,6 +11,11 @@ export default function Splash() {
         <p>
           an integration client for Canvas and Notion
         </p>
+        <Stack style={{width: '100%', height: '25vh'}} direction="row" spacing={2}>
+          <img style={{width: '7rem', height: '6rem'}} src={require('../assets/canvas_logo.png')} />
+          <img style={{width: '8rem', height: '7rem'}} src={require('../assets/right-arrow.png')} />
+          <img style={{width: '8rem', height: '7rem'}} src={require('../assets/Notion_app_logo.png')} />
+        </Stack>
     </Stack>
     </Container>
     
