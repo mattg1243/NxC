bcc14b11e569d519c059996e2b4228424ba5410e
Matt Gallucci <mattgallucci97@gmail.com>
2022-03-08 23:35:24 -0800

    relocated register logic to its own file

--- server/routes/registerRouter.js
+++ server/routes/registerRouter.js
@@ -1,23 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../schemas/user.js')
-// import { registerUser } from '../handlers/userHandlers.js';
+const registerUser = require('../handlers/userHandlers');
+
 // when a new user registers 
-// just gonna put the function straight in here until i get better at modular imports
-router.post("/", (req, res) => { 
-  
-    if (req.body.password === req.body.confirmPassword) {
-      try {
-
-      } catch (err) {
-        res.status(500).json(err.message);
-        throw new Error(err);
-      }
-
-    } else {
-      res.status(403).send("Passwords don't match, please confirm and try again.")
-    }
-  }
-);
+router.post("/", (req, res) => { registerUser(req, res); });
 
 module.exports = router;
