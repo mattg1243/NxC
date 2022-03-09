fc1b7e7f7dddb3772b163b523570c649e498643e
Matt Gallucci <mattgallucci97@gmail.com>
2022-03-08 22:51:19 -0800

    basic register route handler outlines

--- server/handlers/userHandlers.js
+++ server/handlers/userHandlers.js
@@ -1,5 +1,10 @@
 import User from "../schemas/user";
 
-const registerUser = async (req, res) => {
+export function registerUser(req, res) {
   // make sure the passwords match and handle when they dont
+  if (req.body.password !== req.body.confirmPassword) {
+    res.json("passwords match")
+  } else {
+    res.status(406).send("Passwords don't match, please confirm and try again.")
+  }
 }
--- server/routes/registerRouter.js
+++ server/routes/registerRouter.js
@@ -1,9 +1,16 @@
 import express from 'express';
 const router = express.Router();
+// import { registerUser } from '../handlers/userHandlers.js';
 // when a new user registers 
-router.post("/", (req, res) => {
-  res.send("post request received");
-  console.log("request received");
-})
+// just gonna put the function straight in here until i get better at modular imports
+router.post("/", (req, res) => { 
+  
+    if (req.body.password === req.body.confirmPassword) {
+      res.json("passwords match")
+    } else {
+      res.status(403).send("Passwords don't match, please confirm and try again.")
+    }
+  }
+);
 
 export default router;
