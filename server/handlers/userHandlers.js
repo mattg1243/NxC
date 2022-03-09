7edc4d7691d834a0b8b910b0b1ad0fca6681473e
Matt Gallucci <mattgallucci97@gmail.com>
2022-03-08 23:35:01 -0800

    register user fucntionality working

--- server/handlers/userHandlers.js
+++ server/handlers/userHandlers.js
@@ -1,10 +1,26 @@
-import User from "../schemas/user.js";
+const User = require('../schemas/user');
 
-export function registerUser(req, res) {
+const registerUser = (req, res) => {
   // make sure the passwords match and handle when they dont
-  if (req.body.password !== req.body.confirmPassword) {
-    res.json("passwords match")
+  if (req.body.password === req.body.confirmPassword) {
+    try {
+      User.register(new User({
+        username: req.body.email,
+      }), req.body.password, (err, user) => {
+        // log an error if present
+        if (err) { console.error(err); }
+        // otherwise, log confirmation
+        console.log(user.username, "has been registered succesfully");
+        res.status(200).send("User registerd succesfully");
+      })
+    } catch (err) {
+      res.status(500).json(err.code);
+      throw new Error(err);
+    }
+
   } else {
-    res.status(406).send("Passwords don't match, please confirm and try again.")
+    res.status(403).send("Passwords don't match, please confirm and try again.")
   }
 }
+
+module.exports = registerUser;
